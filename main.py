ecb1d45ff27dda9e3c8b86ccd5d1d0f32b195d2e
Patrick Gatal <62809624+PatrickIV@users.noreply.github.com>
2022-10-15 08:36:48 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-COEFS_BP = {1: 0.3, 2: 0.4, 3: 0.5}
-COEFS_DL = {1: 0.3, 2: 0.4, 3: 0.5}
-COEFS_SQ = {1: 0.3, 2: 0.4, 3: 0.5}
+COEFS_BP = {1: 1, 2: 1.25, 3: 1.4, 4: 1.55, 5: 1.70, 6: 1.85, 7: 1.92, 8: 2, 9: 2.1, 10: 2.25}
+COEFS_DL = {1: 1.5, 2: 1.75, 3: 2, 4: 2.25, 5: 2.5, 6: 2.75, 7: 3, 8: 3.10, 9: 25, 10: 3.55}
+COEFS_SQ = {1: 1.25, 2: 1.5, 3: 1.75, 4: 2, 5: 2.25, 6:2.5, 7: 2.75, 8: 2.85, 9: 2.95, 10: 3.05}
 TITLES_STRENGTH = {1: "Strength A", 2: "Strength B", 3: "Strength C"}
 TITLES_ENDURANCE = {1: "Endurance A", 2: "Endurance B", 3: "Endurance C"}
 TITLES_AGILITY = {1: "Agility A", 2: "Agility B", 3: "Agility C"}
@@ -26,9 +26,10 @@ class Strength:
         self.squat = squat
 
     def levelStrength(self):
-        goal_weight_bp = self.user.weight * (self.user.level * 0.1 + 0.2)
-        goal_weight_dl = self.user.weight * (self.user.level * 0.1 + 0.2)
-        goal_weight_sq = self.user.weight * (self.user.level * 0.1 + 0.2)
+        if self.user.level
+        goal_weight_bp = self.user.weight * COEFS_BP[self.level]
+        goal_weight_dl = self.user.weight * COEFS_DL[self.level]
+        goal_weight_sq = self.user.weight * COEFS_SQ[self.level]
         avg_goal_weight = (goal_weight_sq + goal_weight_bp + goal_weight_dl) / 3
         avg_weight_lifted = (self.bench_press + self.deadlift + self.squat)
         self.user.exp += avg_weight_lifted / avg_goal_weight
