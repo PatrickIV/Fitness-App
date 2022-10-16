43f32e1ecc9d4cd62d38ad4d27dfa420e1b0902a
danh295 <hudanny295@gmail.com>
2022-10-15 21:36:33 -0400

    Login layout

---
+++ client/src/pages/LoginPage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function LoginPage() {
+  return (
+    <div>
+        <form>
+            <input type="text" placeholder="username/email/phone #" className="userName"></input>
+            <input type="text" className="pswd"></input> 
+        </form>
+
+
+    </div>
+  )
+}
