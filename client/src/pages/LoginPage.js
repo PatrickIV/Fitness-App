94a0244519eb1b7144cda369bc97ce04e86d6593
danh295 <hudanny295@gmail.com>
2022-10-16 02:41:29 -0400

    Added div & comments

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -3,12 +3,13 @@ import React from 'react'
 export default function LoginPage() {
   return (
     <div>
+        <div className="Login">
         <form>
-            <input type="text" placeholder="username/email/phone #" className="userName"></input>
-            <input type="text" className="pswd"></input> 
+              <input type="text" placeholder="username/email/phone #" className="userName"></input>
+              <input type="text" className="pswd"></input> 
+              {/* query database for user profile -> "Profile.js" */}
         </form>
-
-
+      </div>
     </div>
   )
 }
