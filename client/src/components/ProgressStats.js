be2501824048bbfce832478c85589b43017f39c2
danh295 <hudanny295@gmail.com>
2022-10-16 02:40:48 -0400

    Create ProgressStats.js

---
+++ client/src/components/ProgressStats.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function ProgressStats() {
+  return (
+    <div>
+        {/* import from tableau */}
+        <p>placeholder text for progression stats</p>
+    </div>
+  )
+}
