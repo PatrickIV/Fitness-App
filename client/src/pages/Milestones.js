d0bead86e52dcf41497323e88dc89cec67c05fdd
danh295 <hudanny295@gmail.com>
2022-10-16 05:51:07 -0400

    Tab based conditional rendering

--- client/src/pages/Milestones.js
+++ client/src/pages/Milestones.js
@@ -1,9 +1,67 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Streambar from '../components/Streambar';
+import Pow from '../components/achievements/Pow'
+import End from '../components/achievements/End';
+import Agi from '../components/achievements/Agi';
+import Prg from '../components/achievements/Prg';
 
 export default function Milestones() {
-  return (
-    <div>
-        {/* idek pls help me wtf am i doing */}
-    </div>
-  )
+  const [tab, setTab] = useState('p');
+  
+  function handleTabClick(e) {
+    console.log(e.target.id);
+    setTab(e.target.id);
+  }
+
+  if (tab === 'p') {
+    return (
+      <>
+        <div className="sBar">
+          <Streambar currentTab={tab} onTabClick={handleTabClick} />
+        </div>
+        <div className="achievInv">
+          <Pow /> 
+        </div>
+      </>
+    );
+  }
+  
+  if (tab === 'e') {
+    return (
+      <>
+        <div className="sBar">
+          <Streambar currentTab={tab} onTabClick={handleTabClick} />
+        </div>
+        <div className="achievInv">
+          <End /> 
+        </div>
+      </>
+    );
+  }
+
+  if (tab === 'a') {
+    return (
+      <>
+        <div className="sBar">
+          <Streambar currentTab={tab} onTabClick={handleTabClick} />
+        </div>
+        <div className="achievInv">
+          <Agi /> 
+        </div>
+      </>
+    );
+  }
+
+  if (tab === 'prg') {
+    return (
+      <>
+        <div className="sBar">
+          <Streambar currentTab={tab} onTabClick={handleTabClick} />
+        </div>
+        <div className="achievInv">
+          <Prg /> 
+        </div>
+      </>
+    );
+  }
 }
